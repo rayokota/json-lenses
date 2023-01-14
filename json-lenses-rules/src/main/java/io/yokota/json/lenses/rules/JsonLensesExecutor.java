76b0781cd6761810d46c4d82dc56c1da2c688c05
Robert Yokota <rayokota@gmail.com>
2023-01-13 17:40:16 -0800

    Add DOWNGRADE test

--- json-lenses-rules/src/main/java/io/yokota/json/lenses/rules/JsonLensesExecutor.java
+++ json-lenses-rules/src/main/java/io/yokota/json/lenses/rules/JsonLensesExecutor.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.confluent.kafka.schemaregistry.client.rest.entities.RuleMode;
 import io.confluent.kafka.schemaregistry.rules.RuleContext;
 import io.confluent.kafka.schemaregistry.rules.RuleException;
 import io.confluent.kafka.schemaregistry.rules.RuleExecutor;
 import io.yokota.json.lenses.JsonLenses;
 import io.yokota.json.lenses.ops.LensOp;
+import org.apache.avro.data.Json;
 
 import java.util.List;
 
@@ -31,6 +33,9 @@ public class JsonLensesExecutor implements RuleExecutor {
         try {
             String expr = ctx.rule().getExpr();
             List<LensOp> lens = MAPPER.readValue(expr, new TypeReference<>() {});
+            if (ctx.ruleMode() == RuleMode.DOWNGRADE) {
+                lens = JsonLenses.reverse(lens);
+            }
             return JsonLenses.applyLensToDoc(lens, jsonObj, null);
         } catch (JsonProcessingException e) {
             throw new RuleException("Could not parse rule", e);
--- json-lenses-rules/src/test/java/io/yokota/json/lenses/rules/JsonLensesExecutorTest.java
+++ json-lenses-rules/src/test/java/io/yokota/json/lenses/rules/JsonLensesExecutorTest.java
@@ -3,6 +3,7 @@ package io.yokota.json.lenses.rules;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.confluent.kafka.schemaregistry.client.rest.entities.Rule;
+import io.confluent.kafka.schemaregistry.client.rest.entities.RuleMode;
 import io.confluent.kafka.schemaregistry.rules.RuleContext;
 import org.junit.jupiter.api.Test;
 
@@ -14,14 +15,18 @@ public class JsonLensesExecutorTest {
 
     @Test
     public void testRuleExecutor() throws Exception {
-        String message = "{\"name\": \"Alice\",\"ssn\": \"123-45-6789\"}";
-        JsonNode jsonNode = MAPPER.readTree(message);
+        String original = "{\"name\":\"Alice\",\"ssn\":\"123-45-6789\"}";
+        JsonNode jsonNode = MAPPER.readTree(original);
         String expr = "[ { \"type\": \"rename\", \"source\": \"ssn\", \"target\": \"socialSecurityNumber\" } ]";
         Rule rule = new Rule(null, null, null, null, null, expr, null, null, false);
-        RuleContext ctx = new RuleContext(null, null, null, null, null, null, false, null, rule);
+        RuleContext ctx = new RuleContext(null, null, null, null, null, null, false, RuleMode.UPGRADE, rule);
         JsonLensesExecutor executor = new JsonLensesExecutor();
-        Object output = executor.transform(ctx, jsonNode);
+        JsonNode upgraded = (JsonNode) executor.transform(ctx, jsonNode);
         String expected = "{\"name\":\"Alice\",\"socialSecurityNumber\":\"123-45-6789\"}";
-        assertEquals(expected, output.toString());
+        assertEquals(expected, upgraded.toString());
+
+        ctx = new RuleContext(null, null, null, null, null, null, false, RuleMode.DOWNGRADE, rule);
+        JsonNode downgraded = (JsonNode) executor.transform(ctx, upgraded);
+        assertEquals(original, downgraded.toString());
     }
 }
